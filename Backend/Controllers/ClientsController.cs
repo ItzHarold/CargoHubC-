45bd1fa76e7ccc97001782c5870338a20f398377
Charriexx <charlotte.e.kwok@gmail.com>
2025-01-15 11:00:30 +0100

    client sort and filter options

--- Backend/Controllers/ClientsController.cs
+++ Backend/Controllers/ClientsController.cs
@@ -57,5 +57,27 @@ namespace Backend.Controllers.Clients
             _clientService.DeleteClient(id);
             return NoContent();
         }
+
+        [HttpGet("filtered-sorted")]
+        public IActionResult GetClientsFilteredAndSorted(
+            [FromQuery] string? name = null,
+            [FromQuery] string? address = null,
+            [FromQuery] string? city = null,
+            [FromQuery] string? zipCode = null,
+            [FromQuery] string? province = null,
+            [FromQuery] string? country = null,
+            [FromQuery] string? contactName = null,
+            [FromQuery] string? contactPhone = null,
+            [FromQuery] string? contactEmail = null,
+            [FromQuery] string? sortBy = null,
+            [FromQuery] bool sortDescending = false
+        )
+        {
+            var clients = _clientService.GetClientFilteredAndSorted(
+                name, address, city, zipCode, province, country, contactName, contactPhone, contactEmail, sortBy, sortDescending
+            );
+
+            return Ok(clients);
+        }
     }
 }
